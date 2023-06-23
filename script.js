04001c6693adc4d2993922536f6c25813c79cb9e
deygobinda <118388785+deygobinda@users.noreply.github.com>
2023-06-24 03:37:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,14 @@ Array.from(buttons).forEach((button) => {
             document.querySelector('.input').value = string;
         }
         else if (e.target.innerHTML == 'C') {
-           string =  string.substring(0,string.length-1);
+            string = string.substring(0, string.length - 1);
+            document.querySelector('.input').value = string;
+        } 
+        else if (e.target.innerHTML == '%') {
+            string = eval(`${string}/100`);
             document.querySelector('.input').value = string;
         }
         else {
-            console.log(e.target)
             string = string + e.target.innerHTML;
             document.querySelector('.input').value = string;
         }
